c81e02610548b611b0dad0e364f79529c3953853
LuisRFuentes54 <luisrf0540@gmail.com>
2021-02-08 21:08:46 -0400

    Arregla algunos problemas menores

--- src/IRemote.java
+++ src/IRemote.java
@@ -1,5 +1,6 @@
 import java.rmi.Remote; 
-import java.rmi.RemoteException;  
+import java.rmi.RemoteException;
+import java.util.ArrayList;  
 
 public interface IRemote extends Remote {
     boolean userExist(String ci) throws RemoteException;
@@ -7,6 +8,6 @@ public interface IRemote extends Remote {
     boolean authUser(String username, String password) throws RemoteException;
     int createAccount(String ci, float deposit) throws RemoteException;
     boolean permitCreateAccount(String ci) throws RemoteException;
-    String[] getAccounts(String username) throws RemoteException;
-    String[] getTransactions(String account) throws RemoteException;
+    ArrayList<String> getAccounts(String username) throws RemoteException;
+    ArrayList<String> getTransactions(String account) throws RemoteException;
 }
